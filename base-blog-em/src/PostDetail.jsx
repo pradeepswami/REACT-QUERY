068a129cd5fb2482912b7db1a3eebf9293cbe580
pradeep <pradeepswami.plg@gmail.com>
2024-07-20 10:09:59 +0800

    Initial

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,9 +1,21 @@
 import { fetchComments } from "./api";
 import "./PostDetail.css";
+import { useQuery } from "@tanstack/react-query";
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const {data, isError, isLoading, error} = useQuery({
+    queryKey: ["postComments", post.id],
+    queryFn: () => fetchComments(post.id),
+  });
+
+  if(isLoading) {
+    return <h3>Loading comments....</h3>
+  }
+  
+  if(isError) {
+    return <h3>Error ... {error.toString()}</h3>
+  }
 
   return (
     <>
